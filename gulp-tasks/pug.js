6420bba05491b877ad4389e9e99e5a12b3b4f5b4
NastyaLig <nastia.gil@mail.ru>
2021-08-08 06:46:52 +0300

    discount

--- gulp-tasks/pug.js
+++ gulp-tasks/pug.js
@@ -4,9 +4,11 @@ module.exports = function (gulp, options, plugins) {
 
 	return function (cb) {
 		gulp.src(options.path.src.pug)
+			.pipe(plugins.plumber())
 			.pipe(plugins.pug({
 				pretty: true
 			}))
+			.pipe(plugins.plumber.stop())
 			.pipe(gulp.dest(options.path.build.html))
 			.on('end', plugins.browserSync.reload);
 		cb();
